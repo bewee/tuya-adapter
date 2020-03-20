04bea5b8506cef964ec27e8263fe1fe6a60bc4bc
Galveston01 <benedikt.web23@gmail.com>
2020-03-21 00:33:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tuya-adapter
+Tuya Smart Life IoT devices adapter add-on for Mozilla WebThings Gateway
---
+++ package.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+rm -rf node_modules
+
+npm install --production
+
+shasum --algorithm 256 manifest.json package.json *.js LICENSE.md README.md > SHA256SUMS
+
+find node_modules \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+
+TARFILE=`npm pack`
+
+tar xzf ${TARFILE}
+cp -r node_modules ./package
+tar czf ${TARFILE} package
+
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+
+rm -rf SHA256SUMS package
